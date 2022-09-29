0c5b43a011eea476091a1f886410329594c03384
Gabriel Rodrigues do Nascimento <gabriel.nascimentordn@gmail.com>
2022-09-29 13:34:10 -0300

    Menu, Rodapé, home através do Material Ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Home from './paginas/home/Home';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
 import Login from './paginas/login/Login';
 
 function App() {
   return (
     <>
+      <Navbar/>
       <Home/>
-      <Login/>
+      <Footer/>
     </>
   );
 }
