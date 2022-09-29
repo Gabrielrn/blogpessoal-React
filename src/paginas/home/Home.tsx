bb98d18d86ca2394d04f278c77d48bf055d5af8f
Gabriel Rodrigues do Nascimento <gabriel.nascimentordn@gmail.com>
2022-09-29 12:34:14 -0300

    Exemplo de grid e flexbox

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import './Home.css'
+import React from "react";
+import { Paper, Button } from "@material-ui/core";
+import { Box } from "@mui/material";
+import "./Home.css";
 
 function Home() {
   return (
     //<> significa Fragment, é utlizado para retornar uma lista de elementos
     <>
-      <h1 className='titulo'>Home</h1>
-      <img className='img' src="https://sm.ign.com/t/ign_br/news/b/berserk-ma/berserk-manga-to-continue-after-creators-death_pbr4.1200.jpg" alt="Imagem tela inicial"/>
+      <Paper>
+        <Box p={2}>
+          <Box display="flex" justifyContent="center">
+            <h1>Título</h1>
+          </Box>
+          <img src="https://wallpaperaccess.com/full/673914.jpg" alt="Imagem" style={{width: "100%", height: "100%"}}/>
+          <Box display="flex" justifyContent="center" p={2}>
+            <Button variant="contained" color="primary">Texto 1</Button>
+            <Button variant="contained" color="secondary">Texto 2</Button>
+          </Box>
+        </Box>
+      </Paper>
     </>
-  )
+  );
 }
 
 export default Home;
-
