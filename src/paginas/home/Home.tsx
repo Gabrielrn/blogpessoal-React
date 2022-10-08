a23c68c18ae4285e8b5d13dfc0109ba99e2e0067
Gabriel Rodrigues do Nascimento <gabriel.nascimentordn@gmail.com>
2022-10-07 22:18:58 -0300

    Adicionando Estilização na Home, Login e cadastro

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -28,7 +28,7 @@ function Home() {
                 <Grid alignItems="center" item xs={6}>
                     <Box paddingX={20} >
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui as suas frustações como um bruxo!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1} className='btn-modal'>
@@ -38,7 +38,7 @@ function Home() {
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
-                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                    <img src="https://www.warlegend.net/wp-content/uploads/the-witcher-3-wild-hunt.jpg" alt="" width="580px" height="400px" />
                 </Grid>
                 <Grid xs={12} className='postagens'>
                     <TabPostagem/>
