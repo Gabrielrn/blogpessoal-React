b7c762a914f8846cadab8e29e1e9581206a83eb7
Gabriel Rodrigues do Nascimento <79949781+Gabrielrn@users.noreply.github.com>
2023-06-04 16:35:39 -0300

    Update Service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -4,7 +4,7 @@ import { url } from "inspector";
 //o service ele fica responsável por toda a regra de negócio, a maneira que a comunicação do front e do back (api) ocorre, ao fazer uma requisão espera-se que seja retornado um json.
 
 export const api = axios.create({
-  baseURL: 'https://blogpessoal-beil.onrender.com'
+  baseURL: 'https://blogpessoal-yq7y.onrender.com'
 })
 
 // usuarios/logar 
